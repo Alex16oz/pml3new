aef7f2c0ad3f8d4cb615d15d6b832e86ad999266
Alex16oz <85593164+Alex16oz@users.noreply.github.com>
2025-09-19 09:44:39 +0700

    v4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "yuga.ridho.pml3"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "yuga.ridho.pml3"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
